53a9994898452a0c80578b84774209288f7a0a22
Olanrewaju <olanrewajuemma2023@gmail.com>
2025-10-22 09:33:12 +0100

    updated settings

--- string_analyzer/settings.py
+++ string_analyzer/settings.py
@@ -97,7 +97,6 @@ DATABASES = {
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
     )
 }
 
